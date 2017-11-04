efa0938d8ea436d696ab0bf8e20e64ce8823b8c8
Ivan Alburquerque <albur.ivan@outlook.com>
2017-11-04 15:39:57 -0400

    Add 3 new icons

--- assets/icons/icons.js
+++ assets/icons/icons.js
@@ -1,5 +1,17 @@
-import CameraIcon from "camera.icon.js";
+import OverflowIcon from "./overflow.icon.js";
+import CameraIcon from "./camera.icon.js";
+import InboxIcon from "./inbox.icon.js";
+import LikeIcon from "./like.icon.js";
+import ChatIcon from "./chat.icon.js";
+import SendIcon from "./send.icon.js";
+import BookmarkIcon from "./bookmark.icon.js";
 
 export {
-    CameraIcon
+    OverflowIcon,
+    CameraIcon,
+    InboxIcon,
+    LikeIcon,
+    ChatIcon,
+    SendIcon,
+    BookmarkIcon
 }
